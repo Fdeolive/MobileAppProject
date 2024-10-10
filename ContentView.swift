120003717da6b84d87118cbe110b2a842c3a711b
Grace Kinney <112431548+gracekinney@users.noreply.github.com>
2024-10-10 19:34:12 -0400

    Added icon to textfields
    Need to add checkboxes for remember password and enable faceID, link to recover password, figure out how to enable faceID, connect login page to firebase to save login info

--- ContentView.swift
+++ ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
     @State private var wrongEmail = 0
     @State private var wrongPass = 0
     @State private var showLoginScreen = false
+    
     var body: some View {
+        
         NavigationView {
             // Login Page UI
             ZStack {
@@ -28,29 +30,34 @@ struct ContentView: View {
                 Circle()
                     .scale(1.35)
                     .foregroundColor(.white)
-               
-                // Content: title, email and password textfields, login button
-               // TODO: update styling anf functionality to match wireframe
+                
                 VStack {
                     Text("Welcome to Book Hunter").font(.largeTitle).bold()
                         .padding()
-
-                   // Login and Register Text
+                    
                     HStack {
                         
                         // TODO: setup functionality so that if user is on login screen, the login text will be blue and underlined, and register text will be grey
-                        Text("Login").padding() .foregroundColor(.blue).padding()
+                        Text("Login").padding().underline() .foregroundColor(.blue).padding()
                         
                         Text("Register").padding()
                     }
                     
-                    TextField("Email Address", text: $email).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10).border(.red,   width: CGFloat(wrongEmail));                  SecureField("Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10).border(.red, width: CGFloat(wrongPass))
-
-                  // Login Button
-                    Button("Login"){
-                        
-                    }.foregroundColor(.white).bold().frame(width: 300, height: 50).background(Color.green).cornerRadius(10)
-                }
+                    HStack {
+                        Image(systemName: "envelope").foregroundColor(.black.opacity(0.5))
+                        TextField("Email Address", text: $email)}.padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10).border(.red,   width: CGFloat(wrongEmail))
+                    
+                    HStack{
+                        Image(systemName: "lock").foregroundColor(.black.opacity(0.5))
+                        SecureField("Password", text: $password)
+                        Image(systemName: "eye").foregroundColor(.black.opacity(0.5))}.padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10).border(.red, width: CGFloat(wrongPass))
+                    
+                    
+                        Button("Login"){
+                            
+                        }.foregroundColor(.white).bold().frame(width: 300, height: 50).background(Color.green).cornerRadius(10)
+                    }
+                
             }
             .navigationBarHidden(true)
         }
