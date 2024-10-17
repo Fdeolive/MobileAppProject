a48e1a4a260c24d04e88d7e094c9f6788b4cd55c
Carson King <rozpor127@gmail.com>
2024-10-17 10:01:46 -0400

    Added XCode Project to begin connecting FB to.

---
+++ MobileAppProject/MobileAppProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileAppProject
+//
+//  Created by user267577 on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileAppProject/MobileAppProject/MobileAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppProjectApp.swift
+//  MobileAppProject
+//
+//  Created by user267577 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
