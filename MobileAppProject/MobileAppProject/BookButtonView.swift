59a4a818bdde9d371f0d237f044a6b8b687e2d29
David <dpoulin@uvm.edu>
2024-10-19 12:05:44 -0700

    Testing image stuff

--- MobileAppProject/MobileAppProject/BookButtonView.swift
+++ MobileAppProject/MobileAppProject/BookButtonView.swift
@@ -4,26 +4,29 @@
 //
 //  Created by user264275 on 10/17/24.
 //
+//Todo Integrate BookClass
 
 import SwiftUI
 
 struct BookButtonView: View {
     var buttonText: String = ""
+    var image: String = ""
     var action: () -> Void
     var body: some View {
-            Button(action: action) {
+        Button(action: action) {
+            if (image.count <= 0){
                 Text(buttonText)
                     .font(.title)
                     .frame(width: 60, height: 100)
                     .foregroundColor(Color.white)
                     .background(.gray)
-                    //.overlay(RoundedRectangle
-                      //  .stroke(Color.black, lineWidth: 2))
-                    //.cornerRadius(1)
+            }else{
+                Image(image).resizable().aspectRatio(contentMode: .fit).frame(width: 60, height: 100)
+            }
         }
     }
 }
 
 #Preview {
-    BookButtonView(buttonText: "Word", action:{})
+    BookButtonView(buttonText: "Word", image: "", action:{})
 }
