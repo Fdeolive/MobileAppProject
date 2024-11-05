36c96622d007e3d9478d8dc386d4f5018d1b4ed0
Carson King <rozpor127@gmail.com>
2024-11-05 10:58:34 -0500

    Got rid of filter buttons in SearchView. And currently adding a notification button to Notfications View

---
+++ MobileAppProject/MobileAppProject/SingleNotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleNotificationView.swift
+//  MobileAppProject
+//
+//  Created by user267577 on 11/5/24.
+//
+
+import SwiftUI
+
+struct SingleNotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleNotificationView()
+}
