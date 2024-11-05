2cccc20e3345d2ebe4f1b81f2f3b42add8828119
Carson King <rozpor127@gmail.com>
2024-11-05 15:11:31 -0500

    Modified HomeView and the Notification Views so that you may switch between them. Earlier changes to SearchView didn't save or push earlier so still has filter buttons for now and I'll have to recode the simple search :(

--- MobileAppProject/MobileAppProject/NotificationsView.swift
+++ MobileAppProject/MobileAppProject/NotificationsView.swift
@@ -9,39 +9,48 @@ import SwiftUI
 
 struct NotificationsView: View {
     
-    @State var notificationList = [Notification("New Book Release!", "...", 0), Notification("Stacy Added to Her Wishlist", "...", 1), Notification("John Added to His Wishlist", "...", 2), Notification("John Added to His Wishlist", "...", 3), Notification("Daniel Added to His Wishlist", "...", 4)]
+    @EnvironmentObject var currentNotification: Notification
     
+    @State var notificationList = [Notification("New Book Release!", "...", 0), Notification("Stacy Added to Her Wishlist", "...", 1), Notification("John Added to His Wishlist", "...", 2), Notification("John Added to His Wishlist", "...", 3), Notification("Daniel Added to His Wishlist", "...", 4)]
     private let darkerGreen = Color(red: 0/255, green: 150/255, blue: 25/255)
     private let lighterGreen = Color(red: 240/255, green: 255/255, blue: 240/255)
+    
     var body: some View {
         GeometryReader { geometry in VStack {
+            Group() {
+                if currentNotification.notificationId != -1 {
+                    SingleNotificationView()
+                } else {
             HStack {
                 Spacer()
-                Button("Edit", action: {}).font(.title).padding([.trailing]).foregroundStyle(darkerGreen)
+                Button("Edit", action: { }).font(.title).padding([.trailing]).foregroundStyle(darkerGreen)
             }
-            ScrollView(showsIndicators: false) {
-                VStack {
-                    ForEach(notificationList, id: \.self.notificationId) { notification in
+                    ScrollView(showsIndicators: false) {
                         VStack {
-                            HStack {
-                                Text(notification.notificationTitle).font(.title2).padding([.leading], 25).padding([.bottom], 15).padding([.top])
-                                Spacer()
+                            ForEach(notificationList, id: \.self.notificationId) { notification in
+                                VStack {
+                                    HStack {
+                                        Text(notification.notificationTitle).font(.title2).padding([.leading], 25).padding([.bottom], 15).padding([.top])
+                                        Spacer()
+                                    }
+                                    HStack {
+                                        Spacer()
+                                        Button("Click to View Notification", action: { currentNotification.setNotification(notification)
+                                            }).padding([.trailing], 25).foregroundStyle(Color.gray)
+                                    }
+                                }.frame(width: geometry.size.width - 25, height: geometry.size.height / 6)
+                                    .background(lighterGreen).cornerRadius(20)
+                                    .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.green, lineWidth: 2))
+                                    .padding(5)
                             }
-                            HStack {
-                                Spacer()
-                                Button("Click to View Notification", action: {}).padding([.trailing], 25).foregroundStyle(Color.gray)
-                            }
-                        }.frame(width: geometry.size.width, height: geometry.size.height / 6)
-                            .background(lighterGreen).cornerRadius(20)
-                            .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.green, lineWidth: 2))
-                            .padding(5)
+                        }
                     }
                 }
             }
-        }}.padding()
+        }}
     }
 }
 
 #Preview {
-    NotificationsView()
+    NotificationsView().environmentObject(Notification("NA", "NA", -1))
 }
--- MobileAppProject/MobileAppProject/SingleNotificationView.swift
+++ MobileAppProject/MobileAppProject/SingleNotificationView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct SingleNotificationView: View {
+    
+    private let lighterGreen = Color(red: 220/255, green: 255/255, blue: 220/255)
+    @EnvironmentObject var currentNotification: Notification
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in VStack {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button("", systemImage: "clear", action: { currentNotification.notificationId = -1 }).foregroundColor(Color.black).font(.title)
+                }.padding(25)
+                HStack {
+                    Text(currentNotification.notificationTitle).font(.title)
+                    Spacer()
+                }.padding([.leading],25)
+                
+                HStack {
+                    Text(currentNotification.notifcationSummary)
+                    Spacer()
+                }.padding([.leading], 25).padding([.top], 10)
+                Spacer()
+            }.frame(width: geometry.size.width, height: geometry.size.height).background(lighterGreen)
+            
+        }
+        }
     }
 }
 
 #Preview {
-    SingleNotificationView()
+    SingleNotificationView().environmentObject(Notification("NA", "NA", -1))
 }
